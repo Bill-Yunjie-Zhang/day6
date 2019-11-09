712ef1c0ef702e547cd6ef44e44ac6fac3c83924
Bill <207559339@qq.com>
2019-11-09 11:26:56 +0800

    class

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const cityData = require("all-the-cities")
+
+console.log(cityData.filter(e => e.country === "US" || e.country === "ES"))
+console.log(cityData.filter(e => e.country === "US" || e.country === "ES").length)
+console.log(true == 1)
+console.log(false == 0)
+if("false"){
+    console.log('ibfvahekvhdvbhdvdbv')
+}
