6341bbf2f315cee372637c0f812c5969a1edb1c9
Bill <207559339@qq.com>
2019-11-09 13:49:05 +0800

    class over

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const cityData = require("all-the-cities")
+const airport = require("airport-data")
 
-console.log(cityData.filter(e => e.country === "US" || e.country === "ES"))
-console.log(cityData.filter(e => e.country === "US" || e.country === "ES").length)
-console.log(true == 1)
-console.log(false == 0)
-if("false"){
-    console.log('ibfvahekvhdvbhdvdbv')
-}
+// console.log(cityData.filter(e => e.country === "US" || e.country === "ES"))
+// console.log(cityData.filter(e => e.country === "US" || e.country === "ES").length)
+// console.log(true == 1)
+// console.log(false == 0)
+// if("false"){
+//     console.log('ibfvahekvhdvbhdvdbv')
+// }
+
+// console.log(cityData.find(e => e.name === "Chengdu"))
+// console.log(cityData.filter(e => e.loc.coordinates[1] > 0))
+
+console.log(airport.filter(e => e.city === "Toronto" && e.country === "Canada"))
